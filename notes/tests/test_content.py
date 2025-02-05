5acc30dcd9861f164ffc38e4ba5e62b5495b03fa
Ayrat Khasanov <aihasanov87@gmail.com>
2025-02-05 20:17:19 +0300

    Добавлены теста маршрутов и контекста

---
+++ notes/tests/test_content.py
@@ -0,0 +1,72 @@
+
+from datetime import datetime, timedelta
+from django.conf import settings
+from django.test import TestCase
+from django.utils import timezone
+from django.urls import reverse
+# Импортируем функцию reverse(), она понадобится для получения адреса страницы.
+
+# Импортируем функцию для получения модели пользователя.
+from django.contrib.auth import get_user_model
+
+# Импортируем класс формы.
+from notes.forms import NoteForm
+from notes.models import Note
+
+
+User = get_user_model()
+
+
+class TestHomePage(TestCase):
+    # Вынесем ссылку на домашнюю страницу в атрибуты класса.
+    LIST_URL = reverse('notes:list')
+
+    @classmethod
+    def setUpTestData(cls):
+        # Создаем новости, для проверки, что они будут оботражаться
+        cls.author = User.objects.create(username='Комментатор')
+
+        Note.objects.create(author=cls.author,
+                            title='Новость',
+                            text='Просто текст.',
+                            slug='slugname')
+
+    def test_news_count(self):
+        # авторизуемся и идем на страницу постов
+        self.client.force_login(self.author)
+        response = self.client.get(self.LIST_URL)
+        # Получаем список объектов из словаря контекста.
+        object_list = response.context['object_list']
+        # Определяем количество записей в списке.
+        news_count = object_list.count()
+        # Проверяем, что на странице есть новости.
+        self.assertTrue(news_count > 0)
+
+
+class TestDetailPage(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.author = User.objects.create(username='Комментатор')
+        cls.news = Note.objects.create(
+            author=cls.author,
+            title='Заголовок',
+            text='Текст статьи',
+            slug='slugname'
+        )
+        # Сохраняем в переменную адрес страницы с новостью:
+        cls.detail_url = reverse('notes:add')
+
+    # Проверяем, что аноним не видит форму
+    def test_anonymous_client_has_no_form(self):
+        response = self.client.get(self.detail_url)
+        self.assertIsNone(response.context)
+
+    # Проверяем, что авторизованный видит правильную форму
+    def test_authorized_client_has_form(self):
+        # Авторизуем клиент при помощи ранее созданного пользователя.
+        self.client.force_login(self.author)
+        response = self.client.get(self.detail_url)
+        self.assertIn('form', response.context)
+        # Проверим, что объект формы соответствует нужному классу формы.
+        self.assertIsInstance(response.context['form'], NoteForm)
---
+++ notes/tests/test_routes.py
@@ -0,0 +1,79 @@
+from http import HTTPStatus
+
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+from notes.models import Note
+
+User = get_user_model()
+
+
+class TestRoutes(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        # Создаём двух пользователей с разными именами:
+        cls.author = User.objects.create(username='Лев Толстой')
+        cls.reader = User.objects.create(username='Читатель простой')
+        # От имени одного пользователя создаём новость
+        cls.news = Note.objects.create(
+            author=cls.author,
+            title='Заголовок',
+            text='Текст статьи',
+            slug='slugname'
+        )
+    # Проверяем доступы до страниц без авторизации.
+
+    def test_pages_availability(self):
+        urls = (
+            ('notes:home', None),
+            ('users:login', None),
+            ('users:logout', None),
+            ('users:signup', None),
+        )
+        for name, args in urls:
+            with self.subTest(name=name):
+                url = reverse(name, args=args)
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    # Проверяем доступы до страниц с авторизацией
+    # + те, что доступны только авторам.
+    def test_availability_for_news_edit_and_delete_and_details(self):
+        users_statuses = (
+            (self.author, HTTPStatus.OK),
+            (self.reader, HTTPStatus.NOT_FOUND),
+        )
+        for user, status in users_statuses:
+            # Логиним пользователя в клиенте:
+            self.client.force_login(user)
+            # Для каждой пары "пользователь - ожидаемый ответ"
+            # перебираем имена тестируемых страниц:
+            URLS = (('notes:edit', (self.news.slug,)),
+                    ('notes:delete', (self.news.slug,)),
+                    ('notes:detail', (self.news.slug,)),)
+            for name, args in URLS:
+                with self.subTest(user=user, name=name):
+                    url = reverse(name, args=args)
+                    response = self.client.get(url)
+                    self.assertEqual(response.status_code, status)
+
+    # Проверка переадресация на авторизацию
+    def test_redirect_for_anonymous_client(self):
+        # Сохраняем адрес страницы логина:
+        login_url = reverse('users:login')
+        # В цикле перебираем имена страниц, с которых ожидаем редирект:
+        URLS = (('notes:edit', (self.news.slug,)),
+                ('notes:delete', (self.news.slug,)),
+                ('notes:detail', (self.news.slug,)),
+                ('notes:add', None),
+                ('notes:list', None),
+                ('notes:success', None))
+        for name, args in URLS:
+            with self.subTest(name=name):
+                # Получаем адрес страницы редактирования или удаления комментария:
+                url = reverse(name, args=args)
+                redirect_url = f'{login_url}?next={url}'
+                response = self.client.get(url)
+                # Проверяем, что редирект приведёт именно на указанную ссылку.
+                self.assertRedirects(response, redirect_url)
